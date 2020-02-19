5eea8f5868f9e935db1ec0ca822ee3ca18cdf0fe
Nurhan Turgut <nurhan@google.com>
2020-02-18 16:03:56 -0800

    utils for running flutter driver with web

---
+++ examples/hello_world/dev/webdriver_test.dart
@@ -0,0 +1,25 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:io' as io;
+
+import 'package:args/command_runner.dart';
+
+import 'chromer_driver.dart';
+
+void main(List<String> args) async {
+  // TODO(nurhan): Start/only install driver depending on arg.
+  // TODO(nurhan): Add more browsers' drivers. Control with command line args.
+  try {
+    await ChromeDriver().start();
+  } on UsageException catch (e) {
+    print(e);
+    io.exit(64); // Exit code 64 indicates a usage error.
+  } catch (e) {
+    rethrow;
+  }
+
+  // Sometimes the Dart VM refuses to quit.
+  io.exit(io.exitCode);
+}
